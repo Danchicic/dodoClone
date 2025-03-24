e0664783464598c85e88af9afe9101000802759d
maratdev0210 <maratforwork1@gmail.com>
2025-03-24 17:08:42 +0300

    Fix Navbar styling errors

--- dodo-frontend/src/components/Navbar.jsx
+++ dodo-frontend/src/components/Navbar.jsx
@@ -1,34 +1,32 @@
-import React from 'react';
-import {Link} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
+import React from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import Button from "./UI/Button.jsx";
-import {openCart} from "../redux/cartSlice.js";
-
+import { useState } from "react";
+import Cart from "./Cart.jsx";
 
 const Navbar = () => {
-    const region = useSelector((state) => state.region);
-    const dispatch = useDispatch();
-    return (
-        <div>
-            <nav
-                className="top-0 left-0 w-full sticky flex flex-row gap-4 px-10 items-center backdrop-blur-md bg-white/30 justify-between">
-                <Link to={`/${region || ""}`}>
-                    <img
-                        className="w-30"
-                        src="logo.png"
-                        alt="logo"
-                    />
-                </Link>
-                <div className="flex gap-5 items-center">
-                    <Link to="/profile">Profile</Link>
-                    <Button onClick={
-                        () => dispatch(openCart())
-                    }>Корзина</Button>
-                </div>
-            </nav>
-        </div>
+  const region = useSelector((state) => state.region);
+  const [showCart, setShowCart] = useState(false);
 
-    );
+  return (
+    <div className="w-full top-0 left-0 sticky">
+      <nav className="flex px-10 items-center justify-between">
+        <div>
+          <Link to={`/${region || ""}`}>
+            <img className="w-30" src="logo.png" alt="logo" />
+          </Link>
+        </div>
+        <div>
+          <div className="flex gap-5 items-center relative">
+            <Link to="/profile">Profile</Link>
+            <Button onClick={() => setShowCart(true)}>Корзина</Button>
+            <Cart showCart={showCart} setShowCart={setShowCart} />{" "}
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
