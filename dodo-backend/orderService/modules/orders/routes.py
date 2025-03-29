0a38a06d4945db9fe582bc504b8c1d8809599667
danek52 <guskir3565@gmail.com>
2025-03-29 10:14:03 +0300

    update create order ep

--- dodo-backend/orderService/modules/orders/routes.py
+++ dodo-backend/orderService/modules/orders/routes.py
@@ -1,22 +1,33 @@
+import logging
+
+import aiokafka
 from fastapi import APIRouter, HTTPException, status, Depends
 
 from kafka_broker.producer import KafkaProducer, get_producer
 from . import schemas
 
 router = APIRouter()
+logger = logging.getLogger(__name__)
 
 
 @router.post("/create_order")
 async def create_order(
+        region: str,
         user_order: schemas.UserOrder,
         kafka: KafkaProducer = Depends(get_producer)
 ):
+
     try:
-        await kafka.send_message("orders", user_order.model_dump())
-    except Exception:
+        await kafka.send_message(region, user_order.model_dump())
         raise HTTPException(
             status_code=status.HTTP_201_CREATED,
             detail="Order was created successfully"
+        )
+    except aiokafka.errors.KafkaTimeoutError:
+        logger.exception("Failed to create order in kafka")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Failed to create order"
         )
 
 
